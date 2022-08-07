bfb9607edf9812d5556688119cb52459673e1c40
Urii98 <dendritadev@gmail.com>
2022-08-07 13:30:16 +0200

    07-08: Min 40:10

---
+++ Plataformas2D_SoloUnity/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [Header("Movimiento")]
+    public float moveSpeed;
+
+    [Header("Salto")]
+    private bool canDoubleJump;
+    public float jumpForce;
+
+
+    [Header("Componentes")]
+    public Rigidbody2D theRB;
+
+    [Header("Grounded")]
+    private bool isGrounded;
+    public Transform groundCheckPoint;
+    public LayerMask whatIsGround;
+
+
+    void Start()
+    {
+        
+    }
+
+    
+    void Update()
+    {
+        theRB.velocity = new Vector2(moveSpeed * Input.GetAxis("Horizontal"), theRB.velocity.y);
+
+        isGrounded = Physics2D.OverlapCircle(groundCheckPoint.position, .2f, whatIsGround);
+        //Checks if a Collider falls within a circular area.
+        //The circle is defined by its centre coordinate in world space and by its radius. The optional layerMask allows the test to check only for objects on specific layers.
+
+        if(isGrounded)
+        {
+            canDoubleJump = true;
+        }
+
+        if (Input.GetButtonDown("Jump"))
+        {
+            if(isGrounded)
+            {
+                theRB.velocity = new Vector2(theRB.velocity.x, jumpForce);
+            }
+            else
+            {
+                if(canDoubleJump)
+                {
+                    theRB.velocity = new Vector2(theRB.velocity.x, jumpForce);
+                    canDoubleJump = false;
+                }
+            }
+            
+        }
+    }
+}
